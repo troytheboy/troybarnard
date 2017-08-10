9a0774030c642c2c72705e2d5b95e80d395cc7b5
troytheboy <tbarn2@uw.edu>
2017-08-09 17:31:37 -0700

    animated pic, title, and icons

--- js/app.js
+++ js/app.js
@@ -1,20 +1,12 @@
-var troyApp = angular.module('TroyApp' , ['ngSanitize', 'ui.router', 'ui.bootstrap'])
-    .config(function($stateProvider) {
-        $stateProvider.
-        state('home', {
-            url: '/',
-            templateUrl: 'partials/home.html',
-            controller: 'TroyCtrl'
-        })
-            .config(function($urlRouterProvider){
-                // if the path doesn't match any of the urls you configured
-                // otherwise will take care of routing the user to the specified url
-                $urlRouterProvider.otherwise('/');
-            })
-            .controller('TroyCtrl', ['$scope', '$http', function($scope, $http) {
+console.log("sup");
+function shake(s) {
+  console.log('#' + s);
+  $('#'+s).addClass('animated pulse');
+  setTimeout(function(){ $('#'+s).removeClass('animated pulse')}, 1000);
+  ;
+}
 
-            }]);
-    });
-
-
-
+function show(s) {
+  console.log(s);
+  $('#'+s).show();
+}
