774080800766008456f845da6e9ae59d4f96c45d
troytheboy <tbarn2@uw.edu>
2018-02-27 09:04:31 -0800

    added gulp

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+gulp.task('sass', function() {
+  return sass('css/main.scss')
+    .pipe(gulp.dest('css'))
+    .pipe(reload({ stream:true }));
+});
+
+// watch Sass files for changes, run the Sass preprocessor with the 'sass' task and reload
+gulp.task('serve', ['sass'], function() {
+  browserSync({
+    server: {
+      baseDir: '.'
+    }
+  });
+
+  gulp.watch('css/*.scss', ['sass']);
+});
--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
 function pixar(id, animation) {
   $('#'+id).addClass('animated'+' '+animation);
-  setTimeout(function(){ $('#'+id).removeClass('animated'+' '+animation)}, 1000);
+  setTimeout(function(){ $('#'+id).removeClass('animated'+' '+animation)}, 1000)
 }
 
 function show(s) {
   $('html,body').animate({
-        scrollTop: $("#"+s+'_info').offset().top},'slow');
+        scrollTop: $("#"+s+'_info').offset().top},'slow')
 }
+
+function fadeHeader() {
+  console.log('hide', $('.welcome'))
+  $("div.welcome").hide()
+}
+
+fadeHeader()
